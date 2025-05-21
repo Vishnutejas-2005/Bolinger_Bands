d54d3bbfa540ec37fab4de31ccb22fb61a0decec
Vishnutejas-2005 <vishnutejas@iisc.ac.in>
2025-05-21 09:31:18 +0530

    added some additional statistical tests

--- helpers.py
+++ helpers.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from arch.bootstrap import SPA
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import acf, pacf
+import warnings
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.stats.diagnostic import acorr_ljungbox
+
 
 
 def Dataset_XLS_To_CSV (path:str, Filename:str, col:int)->bool:
@@ -39,7 +48,6 @@ def Dataset_XLS_To_CSV (path:str, Filename:str, col:int)->bool:
         print(f"Exception: {e}")
         return False
     
-
 def get_avg_std(prices:np.ndarray,period:int)->tuple:
     '''
         Takes a numpy array of prices and gives the moving average 
@@ -128,12 +136,13 @@ def buy_and_sell_profits_basic(closing_prices:np.ndarray, upper_band:np.ndarray,
     list_of_profit.append(profit)
     return list_of_profit
 
-def plot_profit(list_of_profit:np.ndarray, period:int = 20,k:int = 2):
+def plot_profit(list_of_profit:np.ndarray, type_of_data:str = "timeseries", period:int = 20, k:int = 2):
     '''
         Takes a numpy array of profits and plots the profits \\
             **********
             #### INPUT
             # list_of_profit - a numpy array of profits \\
+            # type_of_data - a string, the type of data to be plotted
             # period - a int, the period for which the moving 
                     average and std deviation is to be calculated
             # k - a int, the number of standard deviations to be used
@@ -141,12 +150,20 @@ def plot_profit(list_of_profit:np.ndarray, period:int = 20,k:int = 2):
             # #### OUTPUT
             # None
     '''
-    plt.plot(list_of_profit)
-    plt.title(f"Profits made by buying and selling with period {period} and k {k}")
-    plt.xlabel("Time")
-    plt.ylabel("Profit")
-    plt.show()
-
+    if type_of_data == "timeseries":
+        plt.figure(figsize=(10, 5))
+        plt.plot(list_of_profit)
+        plt.title(f"Profits made by buying and selling with period {period} and k {k}")
+        plt.xlabel("Time")
+        plt.ylabel("Profit")
+        plt.show()
+    elif type_of_data == "profit":
+        plt.plot(list_of_profit)
+        plt.title(f"Profits made by buying and selling with period {period} and k {k}")
+        plt.xlabel("Time")
+        plt.ylabel("Profit")
+        plt.show()
+    return None
 
 def generate_list_of_profit(closing_prices:np.ndarray, period:int = 20, k:int = 2):
     '''
@@ -165,3 +182,107 @@ def generate_list_of_profit(closing_prices:np.ndarray, period:int = 20, k:int = 
     upper_band, lower_band = construct_upper_lower_bands(closing_prices, moving_avg, moving_std, period=period)
     list_of_profit = buy_and_sell_profits_basic(closing_prices, upper_band, lower_band, moving_avg)
     return list_of_profit
+
+def get_spa(list_of_returns:np.ndarray, benchmark_index: int = 0, alpha:float = 0.05):
+    '''
+        Takes a numpy array of profits and gives the SPA test results \\
+            **********
+            #### INPUT
+            # list_of_profit - a numpy array of profits \\
+            # benchmark_index - a int, the index of the benchmark to be used
+            # alpha - a float, the significance level to be used
+            **********
+            # #### OUTPUT
+            # tuple - a tuple of the SPA test results
+    '''
+    benchmark_returns = list_of_returns[:, benchmark_index]
+    # everything else
+    stratergy_returns = np.delete(list_of_returns, benchmark_index, axis=1)
+    benchmark_losses = -benchmark_returns
+    stratergy_losses = -stratergy_returns
+    spa = SPA(benchmark=benchmark_losses,
+        models=stratergy_losses,
+        reps=1000,
+        block_size=100,  
+        bootstrap="stationary",
+        studentize=True,
+        nested=False,
+        seed=42
+    )
+    spa.compute()
+    return spa
+
+def interpret_spa(spa, alpha:float = 0.05, benchmark_index: int = 0):
+    '''
+        Takes the computed spa and gives the interpretation of the results \\
+            **********
+            #### INPUT
+            # spa - the computed spa
+            # alpha - a float, the significance level to be used
+            # benchmark_index - a int, the index of the benchmark to be used
+            **********
+            # #### OUTPUT
+            # indices_of_significant_models - a list of the indices of the models that are significantly different from the benchmark
+    '''
+    result = spa.better_models(pvalue=0.05, pvalue_type="consistent")
+    # add 1 to the result whose index is more than i and do nothing for the rest
+    result = [x + 1 if x > benchmark_index else x for x in result]
+    return result
+
+def ad_fuller_test(data:np.ndarray):
+    '''
+        Takes a numpy array of timeseries and gives the adfuller test results \\ 
+         \\ preferebly returns percentage \\
+            **********
+            #### INPUT
+            # data - a numpy array of data
+            **********
+            # #### OUTPUT
+            # tuple - a tuple of the adfuller test results
+    '''
+    result = adfuller(data)
+    return result
+
+def ljung_box_test(data:pd.Series, lags:list = [10]):
+    '''
+        Takes a dataframe of data and gives the ljung box test results \\
+            **********
+            #### INPUT
+            # data - a dataframe of data
+            # lags - a int, the number of lags to be used
+            **********
+            # #### OUTPUT
+            # dataframe - a dataframe of the ljung box test results
+    '''
+    result = acorr_ljungbox(data, lags=lags)
+    return result
+
+def plot_acf_pacf(data:pd.Series, lags:int = 10, change_the_scale:bool = False):
+    '''
+        Takes a numpy array of data and gives the acf and pacf plots \\
+            **********
+            #### INPUT
+            # data - a numpy array of data
+            # lags - a int, the number of lags to be used
+            # change_the_scale - a boolean, if True, the scale of the plot is changed
+            **********
+            # #### OUTPUT
+            # None
+    '''
+    plt.figure(figsize=(12, 6))
+    plot_acf(data, lags=lags)
+    if change_the_scale:
+        plt.ylim(-0.1, 0.1)
+    plt.title('ACF Plot')
+    plt.show()
+    plt.figure(figsize=(12, 6))
+    plot_pacf(data, lags=lags)
+    if change_the_scale:
+        plt.ylim(-0.1, 0.1)
+    plt.title('PACF Plot')
+    plt.show()
+    return None
+
+
+
+
--- setup.py
+++ setup.py
@@ -6,19 +6,44 @@ import matplotlib.pyplot as plt
 
 list_of_periods = [5, 10, 15, 20, 25, 30]
 list_of_k = [1, 1.5, 2]
+
+
 # Dataset_XLS_To_CSV('cumul_ohlc.xls', 'Dataset', 10)
+
+
 
 # get the closing prices data from Dataset.csv
 data = pd.read_csv('Dataset.csv')
 closing_prices = np.array(data['close'].values)
 
-list_of_profits = []
-for k in list_of_k:
-    for period in list_of_periods:
-        list_of_profits.append(generate_list_of_profit(closing_prices, period, k))
-        print(f"Profit for period {period} and k {k} is completed")
 
-np.save('profits.npy', list_of_profits)
-print("list_of_profits saved to profits.npy")
+
+# list_of_profits = []
+# for k in list_of_k:
+#     for period in list_of_periods:
+#         list_of_profits.append(generate_list_of_profit(closing_prices, period, k))
+#         print(f"Profit for period {period} and k {k} is completed")
+
+# np.save('profits.npy', list_of_profits)
+# print("list_of_profits saved to profits.npy")
         
 
+
+list_of_profits = np.load('profits.npy', allow_pickle=True)
+print("list_of_profits loaded from profits.npy")
+print(list_of_profits.shape)
+
+
+list_of_returns = []
+for i in range(len(list_of_profits)):
+    list_of_returns.append(np.diff(list_of_profits[i]))
+
+# reshape the list_of_returns to its transpose
+list_of_returns = np.array(list_of_returns).T
+
+for i in range(list_of_returns.shape[1]):
+    spa = get_spa(list_of_returns=list_of_returns, benchmark_index=i, alpha=0.05)
+    significant_better_models = interpret_spa(spa, alpha=0.05, benchmark_index=i)
+    print(f"\nBetter models than benchmark (strategy {i}):")
+    print(significant_better_models)
+    
